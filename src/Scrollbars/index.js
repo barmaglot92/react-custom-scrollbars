ffbdc97e6a256f6f3806cb092b46fcb62f6a9959
Andrey Zhvakin <barmaglot92@gmail.com>
2019-04-22 19:14:59 +0300

    in pr

--- src/Scrollbars/index.js
+++ src/Scrollbars/index.js
@@ -508,6 +508,7 @@ export default class Scrollbars extends Component {
             autoHeightMax,
             style,
             children,
+            defaultScrollbarWidth,
             ...props
         } = this.props;
         /* eslint-enable no-unused-vars */
